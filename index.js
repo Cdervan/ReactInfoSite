7e5b620e792394a1503e52bc000692b3517781b7
Christopher Dervan <Cdervan99@gmail.com>
2022-03-25 20:29:38 -0400

    create base static react site

--- index.js
+++ index.js
@@ -1,34 +1,15 @@
-/** const h1 = document.createElement("h1")
-    h1.textContent = "This is an imperative way to program"
-    h1.className = "header"
-    document.getElementById("root").append(h1) **/
-
-function HomeList() {
-    return (
-        <ul>
-            <li>Dog water</li>
-            <li>Goat malk</li>
-            <li>Bird flu</li>
-            <li>Penguin juice</li>
-        </ul>
-    )
-}
-//ReactDOM.render( <HomeList />, document.getElementById('root'))
-
-//const element = <h1 className="header"><HomeList /></h1>
-
 const page = (
     <div>
-        <h1 className="header">JFK was assassinated by the CLintons</h1>
-        <p>This is a paragraph</p>
+        <img src="./react-logo.png" width="100"></img>
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on Github</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
 )
-
-//ReactDOM.render(element, document.getElementById("root"))
-
-ReactDOM.render(
-    page,
-    document.getElementById("root")
-)
-console.log(page)
+ReactDOM.render(page, document.getElementById("root"))
 
