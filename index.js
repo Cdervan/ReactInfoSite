c7fcca9809d9a6eb375e87bfcb8e9bcc199bb4e6
Christopher Dervan <Cdervan99@gmail.com>
2022-03-26 20:11:20 -0400

    Split page() into components and style changes

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 function Page() {
     return (
         <div>
-        <img src="./react-logo.png" width="100"></img>
-        <h1>Fun facts about React</h1>
-        <ul>
-            <li>Was first released in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on Github</li>
-            <li>Is maintained by Facebook</li>
-            <li>Powers thousands of enterprise apps, including mobile apps</li>
-        </ul>
-    </div>
+            <img src="./react-logo.png" width="100"></img>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on Github</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <header className="header">
+            <nav className="nav">
+                <img className="nav-logo" src="./react-logo.png" width="40px" />
+                <ul className="nav-items">
+                    <li>Videos</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+function Body() {
+    return (
+        <div>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+            <li>Spice up my resume with an in demand skill</li>
+                <li>learn javascript by proxy</li>
+                <li>can finally complete project that requires react to function</li>
+                <li>money</li>
+            </ol>
+        </div>
+    )
+}
+function Footer() {
+    return (
+        <footer className="footer">
+            <small className="footerText">© 2022 Dervan development. All rights reserved.</small>
+        </footer>
+    )
+}
+function Page() {
+    return (
+        <div>
+            <Header />
+            <Body />
+            <Footer />
+        </div>
     )
 }
 
