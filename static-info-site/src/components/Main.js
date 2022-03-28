738751512af5edbab2128b4377e8eed744c75e9e
Christopher Dervan <Cdervan99@gmail.com>
2022-03-27 20:23:42 -0400

    adding style to body

--- static-info-site/src/components/Main.js
+++ static-info-site/src/components/Main.js
@@ -2,8 +2,15 @@ import React from "react"
 
 export default function Main() {
     return (
-        <div className="body-container">
-            <h1>Main component</h1>
-        </div> 
+        <div className="main-content">
+            <h1 className="facts-title">Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k start on GithuB</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </div>
     )
 }
--- static-info-site/src/components/Navbar.js
+++ static-info-site/src/components/Navbar.js
@@ -3,8 +3,8 @@ import reactLogo from "../images/react-logo.png"
 
 export default function Navbar() {
     return (
-        <div className="container">
-            <img className="react-logo" src={reactLogo} width="40px"/>
+        <div className="nav">
+            <img className="react-logo" src={reactLogo} width="50px"/>
             <h1>React Facts</h1>
             <h2>React Course - Project 1</h2>
         </div>
